b4d436d05c8fb77c0148cdd0e906d875db123e64
John Clarke <john@algofi.org>
2022-02-07 03:48:44 +0000

    added package dunders

--- algofi/__init__.py
+++ algofi/__init__.py
@@ -1,1 +1,7 @@
-version="0.0.7"
+"""
+This module contains all the relevant classes and data for interacting with the Algofi Lending Protocol
+"""
+
+__all__ = ["v1", "contract_strings", "utils"]
+__version__ = "0.0.7"
+__author__ = "Algofi"
